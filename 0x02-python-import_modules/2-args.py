03676a13295822ea6df949206fc3837d6cc8edf7
james <arizonahjames@gamil.com>
2023-11-30 01:47:21 -0800

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+import sys
+
+
+def main(argv):
+
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(argv) - 1 == 1:
+        print("{:d} argument:".format(len(argv) - 1))
+        print("{:d}: {}".format(len(argv) - 1, argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i, x in enumerate(argv[1:], 1):
+            print("{:d}: {}".format(i, x))
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    main(sys.argv)
